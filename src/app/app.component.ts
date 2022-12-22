a990075b9997ad1c40c64686484e677493d085a0
bryanrodrguez <b.rodrguezsuazo@uandresbello.edu>
2022-12-22 00:14:09 -0300

    TryPiano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Proyecto_Piano';
+  aplicarSonido(numero:number):void{
+    const audio= new Audio();
+    audio.src = '../assets/Sonidos/note'+numero+'.wav';
+    audio.load();
+    audio.play();
+  }
 }
